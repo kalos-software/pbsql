775948d79b0a033befba2cee1661c978f99dbeff
Robbie Milejczak <robbie@kalosflorida.com>
2019-10-03 11:24:38 -0400

    adding derived alias for subquery in BuildCountQuery

--- main_test.go
+++ main_test.go
@@ -16,6 +16,14 @@ type TestStruct struct {
 
 var target TestStruct
 
+var expectedCreateQry = "INSERT INTO test_table (test_table.date, test_table.geolocation_lat, test_table.geolocation_lng) VALUES (?, ?, ?)"
+
+var expectedReadQry = "SELECT test_table.id, ifnull(test_table.name, '') as name, ifnull(test_table.date, '') as date, ifnull(test_table.geolocation_lat, 0.0) as geolocation_lat, ifnull(test_table.geolocation_lng, 0.0) as geolocation_lng, test_table.is_active FROM test_table WHERE true AND test_table.id = ? AND test_table.date LIKE ? AND test_table.geolocation_lat = ? AND test_table.geolocation_lng = ?"
+
+var expectedUpdateQry = "UPDATE test_table SET test_table.geolocation_lat = ?, test_table.geolocation_lng = ? WHERE test_table.id = ?"
+
+var expectedDeleteQry = "UPDATE test_table SET test_table.is_active = ? WHERE test_table.id = ?"
+
 func TestMain(m *testing.M) {
 	target.ID = 1
 	target.Date = "2019-01-01"
@@ -29,7 +37,11 @@ func TestBuildCreate(t *testing.T) {
 	if err != nil {
 		t.Fatal("BuildCreateQuery failed", err)
 	}
-	println(qry)
+
+	if qry != expectedCreateQry {
+		t.Log("Got: ", qry)
+		t.Fatal("Expected:", expectedCreateQry)
+	}
 }
 
 func TestBuildRead(t *testing.T) {
@@ -37,7 +49,11 @@ func TestBuildRead(t *testing.T) {
 	if err != nil {
 		t.Fatal("BuildReadQuery failed", err)
 	}
-	println(qry)
+
+	if qry != expectedReadQry {
+		t.Log("Got:", qry)
+		t.Fatal("Expected:", expectedReadQry)
+	}
 }
 
 func TestBuildUpdate(t *testing.T) {
@@ -48,7 +64,12 @@ func TestBuildUpdate(t *testing.T) {
 	if err != nil {
 		t.Fatal("BuildUpdateQuery failed", err)
 	}
-	println(qry)
+
+	if qry != expectedUpdateQry {
+		t.Log("Got:", qry)
+		t.Fatal("Expected:", expectedUpdateQry)
+	}
+
 }
 
 func TestBuildDelete(t *testing.T) {
@@ -56,5 +77,9 @@ func TestBuildDelete(t *testing.T) {
 	if err != nil {
 		t.Fatal("BuildDeleteQuery failed", err)
 	}
-	println(qry)
+
+	if qry != expectedDeleteQry {
+		t.Log("Got:", qry)
+		t.Fatal("Expected:", expectedDeleteQry)
+	}
 }
