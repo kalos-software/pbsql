2069548e5df1602ade54ecb7fcab8b1724051296
Robbie Milejczak <robbie@kalosflorida.com>
2021-05-12 15:11:41 -0400

    something else

--- helpers.go
+++ helpers.go
@@ -311,7 +311,7 @@ func (qb *queryBuilder) handleForeignKey(f *field) {
 			localName,
 		)
 	}
-	if !related.IsZero() {
+	if !related.IsValid() {
 		qb.handleDateRange(foreignTable, &related)
 	}
 }
